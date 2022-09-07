3135f7f97cf05eeb42c082c6f9f8f24a17b601dd
Ari <atanner@centos.supakoopa.net>
2022-09-07 14:43:24 +0300

    testailua

--- osake.py
+++ osake.py
@@ -1,10 +1,25 @@
-def amountOfStocks(stocks):
-	print(len(stocks))
-	for value in stocks:
-		#print(value)
-		if int(value):
-			print(value)
-		
+def amountOfStocks():
+	stocks = int(input("Amount of stocks: "))
+	return stocks	
 
-amountOfStocks('123k')
+def shareValue():
+	sharevalue = float(input("Share value: "))
+	return sharevalue
+	
+def stockHoldEq():
+	assets = int(input("Total assets: "))
+	liabilities = int(input("Total liabilities: "))
+	stock_eq = assets - liabilities
+
+	return stock_eq
+
+def calcPB():
+	stocks = amountOfStocks()
+	sharevalue = shareValue()
+	book = stockHoldEq()
+	pb_value = sharevalue / (book/stocks)
+	return round(pb_value, 2)
+
+print(calcPB())
+#amountOfStocks('123k')
 
